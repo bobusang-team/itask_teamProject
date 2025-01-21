142d8687a5fdcf4180236b2ef9bfb7d49aa2568f
seungseunga <bluemelody04@icloud.com>
2025-01-21 17:27:58 +0900

    승아 0121 파일 임시저장

--- AskIT/src/main/java/com/itask/app/notice/NoticeFrontController.java
+++ AskIT/src/main/java/com/itask/app/notice/NoticeFrontController.java
@@ -1,5 +1,37 @@
 package com.itask.app.notice;
 
 public class NoticeFrontController {
+	
+	
 
 }
+
+
+// 아래의 내용 참고하여 만들것 ------------------------------------------------------------------
+import org.apache.ibatis.session.SqlSession;
+
+import com.itask.app.dto.UserDTO;
+import com.mybatis.config.MyBatisConfig;
+
+public class UserDAO {
+   public SqlSession sqlSession;
+
+   public UserDAO() {
+      sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+   }
+   
+   // 회원가입
+   public void join(UserDTO userDTO) {
+      sqlSession.insert("user.join", userDTO);
+   }
+   
+   // 로그인
+   public UserDTO login(String userId, String userPw) {
+	   UserDTO userDTO = new UserDTO();
+	   userDTO.setUserId(userId);
+	   userDTO.setUserPw(userPw);
+	   return sqlSession.selectOne("user.login", userDTO);
+   }
+   
+}
+
